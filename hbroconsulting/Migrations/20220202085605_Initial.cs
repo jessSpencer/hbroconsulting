fb1ef93337158e00c862c62c75be76c4d0ed483c
Jessie Spencer <jeskspencer@gmail.com>
2022-02-02 01:57:36 -0700

    final

---
+++ hbroconsulting/Migrations/20220202085605_Initial.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace hbroconsulting.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Responses",
+                columns: table => new
+                {
+                    MovieId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Category = table.Column<string>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    Year = table.Column<int>(nullable: false),
+                    Director = table.Column<string>(nullable: false),
+                    Rating = table.Column<string>(maxLength: 25, nullable: false),
+                    Edited = table.Column<bool>(nullable: false),
+                    LentTo = table.Column<string>(nullable: true),
+                    Notes = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Responses", x => x.MovieId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "MovieId", "Category", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 1, "Family", "Byron Howard", false, "mom", "no", "PG", "Encanto", 2021 });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "MovieId", "Category", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 2, "Comedy", "Akiva Schaffer", true, "Lindy", "no", "PG-13", "Hot Rot", 2007 });
+
+            migrationBuilder.InsertData(
+                table: "Responses",
+                columns: new[] { "MovieId", "Category", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 3, "Horror/Suspence", "John Krasinski", false, "paul", "no", "PG-13", "A Quiet Place", 2018 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Responses");
+        }
+    }
+}
