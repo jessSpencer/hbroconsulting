407d7411db60cdafafabeec3c5f5e635302a8152
Jessie Spencer <jeskspencer@gmail.com>
2022-02-01 19:15:18 -0700

    Add project files.

---
+++ hbroconsulting/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using hbroconsulting.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hbroconsulting.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private hbroMoviesContext _theContext { get; set; }
+
+        //Constructor
+        public HomeController(ILogger<HomeController> logger, hbroMoviesContext name)
+        { 
+            _logger = logger;
+            _theContext = name;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Movies()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Movies(movieresponse mr)
+        {
+            _theContext.Add(mr);
+            _theContext.SaveChanges();
+
+            return View("confirmation", mr);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult MyPodcast()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ hbroconsulting/Models/hbroMoviesContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hbroconsulting.Models
+{
+    public class hbroMoviesContext : DbContext
+    {
+        //Constructor
+        public hbroMoviesContext (DbContextOptions<hbroMoviesContext> options) : base (options)
+        {
+            //Leave blank for now
+        }
+
+        public DbSet<movieresponse> Responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder ab)
+        {
+            ab.Entity<movieresponse>().HasData(
+
+                new movieresponse
+                {
+                    MovieId = 1,
+                    Category = "Family",
+                    Title = "Encanto",
+                    Year = 2021,
+                    Director = "Byron Howard",
+                    Rating = "PG",
+                    Edited = false,
+                    LentTo = "mom",
+                    Notes = "no"
+                }
+            );
+        }
+    }
+}
